1e1263f82b90101681fd42c4be8d57ca9d20674f
RongYu98 <Stuy.rong.yu@gmail.com>
2014-09-24 08:38:58 -0400

    Update StringStuff.java

--- HW-05/StringStuff.java
+++ HW-05/StringStuff.java
@@ -26,3 +26,32 @@ public int diff21(int n) {
 	return (21-n);
 
 }
+
+//Classwork with Kathryn 
+/*Working :)*/
+public boolean lastDigit(int a, int b, int c) {
+  int A = a % 10;
+  int B = b % 10;
+  int C = c % 10;
+  if (A == B) {
+  return true;
+  } else if (A==C){
+  return true;
+  } else if (B==C){
+  return true;
+  } else return false;
+
+}
+
+public boolean nearHundred(int n) {
+  return ((Math.abs(100 - n) <= 10 ||(Math.abs(200 - n) <=10);
+}
+
+public int teaParty(int tea, int candy) {
+   if (tea < 5 || candy < 5) {return 0};
+   if (tea >= (candy * 2) || candy >= (tea * 2)) {
+   	return 2;
+   } else if (tea >= 5 && candy >= 5){
+   	return 0;
+   }
+   }
