6d4cb23b37c7ce068acb49b954dd4ec867ed1771
Rong Yu <Stuy.rong.yu@gmail.com>
2014-10-02 19:38:44 -0400

    This took a while...

--- HW-07/Shapes.java
+++ HW-07/Shapes.java
@@ -55,5 +55,125 @@ public class Shapes {
 			}
 		return  A;
 }
+
+	public String tri3(int h){
+    		int Height = h;
+    		int Column = 1;
+    		int NewColumn = Column;
+   		String S = "";
+   		while (h > 0) {
+			NewColumn = Column;
+        			while ((Height - NewColumn) > 0) {
+        				S = S + ".";
+        				NewColumn = NewColumn + 1; }
+        			NewColumn = Column;
+        			while ((NewColumn - 1) > 0) {
+        				S = S + "*";
+            				NewColumn = NewColumn - 1;}
+        			S = S + "*";
+        			NewColumn = Column;
+        			while ((NewColumn - 1) > 0) {
+        				S = S + "*";
+            				NewColumn = NewColumn - 1;}
+			NewColumn = Column;
+        			while ((Height - NewColumn) > 0) {
+       				S = S + ".";
+        				NewColumn = NewColumn + 1; }
+        			Column = Column + 1;
+        			h = h - 1;
+			S = S + "\n"; }
+    return S;}
+
+
+	public String diamond(int h){
+		int H = h;
+		String S = "";
+		int Counter = 1;
+		int Half = h / 2;
+		int UsingCounter = Counter;
+		int X;
+
+		//BEGINING
+		while (Half > 0) {
+		X = 0;
+		while (((H - Counter) / 2) - X > 0) {
+			S = S + " ";
+			X = X + 1;}
+
+		UsingCounter = Counter;
+		while (UsingCounter > 0) { 
+			UsingCounter = UsingCounter - 1;
+			S = S + "*";}
+
+		X = 0;
+		while (((H - Counter) / 2) - X > 0) {
+			S = S + " ";
+			X = X + 1;}
+
+		S = S + "\n";
+		Half = Half - 1;
+		Counter = Counter + 2; }
+
+		//MIDDLE
+		while (H > 0) {
+			S = S + "*";
+			H = H - 1;}
+
+		S = S + " \n";
+		Half = h / 2;
+		
+		//END
+		Counter = Half + 2;
+		UsingCounter = Counter;
+		X = Half + 1;
+		int x = X;
+		while (Half > 0) {
+			while (Half - X < 0){
+				S = S + " ";
+				X = X - 1;}
+		
+		while (UsingCounter > 0) {
+			S = S + "*";
+			UsingCounter = UsingCounter - 1; }
+
+		X =x;
+			while (Half - X < 0){
+				S = S + " ";
+				X = X - 1;}
+		
+		S = S + "\n";
+		Half = Half - 1;
+		X =x;
+		Counter = Counter - 2;
+		UsingCounter = Counter;
+		}
+
+
+	return S;
+	}
+//Is there a easier way? This took me around 2 hours.
+
+
+	public String tri4(int h){
+		String S = "";
+		int H = h;
+		int HH = H;
+		int Counter = 0;
+		int UsingCounter = Counter;
+
+		while (H > 0){
+			HH = H;
+			while (UsingCounter > 0){
+				S = S + " ";
+				UsingCounter = UsingCounter - 1;}
+			while (HH > 0) {
+				S = S + "*";
+				HH = HH - 1;}
+			Counter = Counter + 1;
+			UsingCounter = Counter;
+			S = S + "\n";
+			H = H - 1;}
+
+		return S; }
 } 
 
