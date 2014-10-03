325de710c1482bca7397255c7f2cc7f0f333ece0
Rong Yu <Stuy.rong.yu@gmail.com>
2014-10-03 18:15:06 -0400

    Work

---
+++ HW-08/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+	public static void main(String[] args){
+		StringStuff S = new StringStuff();
+		System.out.print(S.frame(5, 5));
+		System.out.println(S.stringSplosion("ABCDE"));
+		System.out.println(S.stringX("xxxxxxxx"));
+	}
+}
