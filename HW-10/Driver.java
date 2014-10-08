9819f2ae66cfc1d5d61b5ef4b4574cfa594e488b
Rong Yu <Stuy.rong.yu@gmail.com>
2014-10-08 19:34:48 -0400

    class fun?

---
+++ HW-10/Driver.java
@@ -0,0 +1,21 @@
+public class Driver { 
+
+    public static void main(String[] args){ 
+
+	Basechar c = new Basechar(),c2; 
+	Warrior w = new Warrior(),w2; 
+	Mage m = new Mage(),m2; 
+
+	w.setName("Groo"); 
+	m.setName("Mr. Sage"); 
+
+	//w.attack(m); 
+	m.Default("Mage Lord", 99999); 
+	//w.attack(w); 
+
+	//System.out.println(w.toString()); 
+	//System.out.println(w); 
+
+    } 
+
+} 
