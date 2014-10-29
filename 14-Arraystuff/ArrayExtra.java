3a98ebe3d03d4a53a14bcd8d23248cd91e3f96db
Rong Yu <Stuy.rong.yu@gmail.com>
2014-10-29 17:02:07 -0400

    Coding bat problems.

---
+++ 14-Arraystuff/ArrayExtra.java
@@ -0,0 +1,103 @@
+public class ArrayExtra{
+
+
+public int sum67(int[] nums) {
+  boolean is6 = false;
+  int answer=0;
+  for (int x=0; x<nums.length; x++){
+    if (nums[x] == 6){
+      is6 = true;}
+    if (is6){}
+    else {
+      answer = answer + nums[x];}
+    if (nums[x] == 7){
+      is6 = false;}
+      }
+    return answer;
+
+}
+
+
+public boolean tripleUp(int[] nums) {
+  for (int x=2; x<nums.length; x++){
+     if ((nums[x - 2] + 1 == nums[x - 1]) && (nums[x-1] + 1 == nums[x])){
+       return true;}}
+  return false;
+}
+
+
+public boolean more14(int[] nums) {
+  int of1s=0;
+  int of4s=0;
+  for (int x=0; x < nums.length; x++){
+     if (nums[x]==1){
+        of1s = of1s + 1;}
+     else if (nums[x]==4) {of4s = of4s + 1;}}
+  if (of1s>of4s) {
+    return true;
+    } else {return false;}
+}
+
+
+public int[] tenRun(int[] nums) {
+  int first;
+  
+  if (nums.length==0){
+    return nums;}
+  
+  int x=0;
+  while (true){
+    if (nums[x]%10==0){
+      first = x;
+      break;}
+    if (x+1==nums.length){
+      return nums;}
+    x = x + 1;
+    }
+
+  int tener = nums[first];
+  for (x=first; x<nums.length; x++){
+    if (nums[x]%10==0){
+      tener=nums[x];}
+    else {nums[x] = tener;}
+    }
+  return nums;
+}
+
+
+public boolean canBalance(int[] nums) {
+
+  int front = 0;
+  int back = nums.length - 1;
+  int FrontSum = 0;
+  int BackSum = 0;
+  if (nums.length == 1){
+    return false;}
+  while (front<=back){
+    if (FrontSum < BackSum) {
+      FrontSum = FrontSum + nums[front];
+      front = front + 1;}
+    else {
+      BackSum = BackSum + nums[back];
+      back = back - 1;}
+      }
+    return (FrontSum==BackSum);
+}
+
+
+public int[] seriesUp(int n) {
+    int[] intArray = new int[n * (n+1) / 2];
+    
+    int ArrayCounter = 0;
+    
+    for (int x=1; x<=n; x++){
+       for (int counter = 1; counter <= x; counter++){
+          intArray[ArrayCounter]=counter;
+          ArrayCounter = ArrayCounter + 1;
+          }
+       }
+    return intArray;
+}
+
+
+}
