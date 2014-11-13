2721afd620f5398423003c4f64733b03056a1a69
Rong Yu <Stuy.rong.yu@gmail.com>
2014-11-13 18:35:37 -0500

    this is not fun

--- 17-ArrayLists/ArrayLists.java
+++ 17-ArrayLists/ArrayLists.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class ArrayLists{
     public static void main(String[] args){
 
+/////////ArrayList (Works
     int X=0;
     ArrayList<Integer> AL;
     AL = new ArrayList<Integer>();
@@ -21,6 +22,43 @@ public class ArrayLists{
     AL.remove(X);
     }
     //System.out.println(AL);
+
+///////////////Arrays (Can't find bug...)
+/*
+    
+    int Z = 10;
+    int[] IntArray = new int[Z];
+    for (int i=0; i<Z; i++){
+        IntArray[i]=i;}
+    
+    boolean is0up = false;
+    int[] Newbie = IntArray;
+
+    int t=0;
+    while (t<IntArray.length){
+        X=r.nextInt(Z);
+        if (!(IntArray[X]==(0))){
+            Newbie[t]=IntArray[X];
+            IntArray[X]=0;
+            t++;}
+        else if(!is0up){
+            Newbie[t]=IntArray[X];
+            is0up=true;
+            t++;
+        } 
+    }
+
+    for (int i=0; i<Newbie.length; i++){
+        System.out.print(Newbie[i]);
+    }
+    //System.out.println();
+    for (int i=0; i<Newbie.length; i++){
+        System.out.print(IntArray[i]);
+    }
+    //System.out.println();
+
+*/
+
 
 }
 }
