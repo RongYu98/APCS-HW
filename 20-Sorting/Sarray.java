a4575f83b557c1c02896ee7ebad8840dd86636ee
rong.yu <stuy.rong.yu@gmail.com>
2014-12-04 08:40:23 -0500

    yay, this was easy

--- 20-Sorting/Sarray.java
+++ 20-Sorting/Sarray.java
@@ -30,6 +30,33 @@ public  class Sarray{
 	return true;
     }
     
+    public void sSort(){
+	String smallest;
+	for (int i=0; i<data.length; i++){
+	    int n=i;
+	    smallest=data[i];
+	    for (int z=i; z<data.length; z++){
+		if (smallest.compareTo(data[z])>0){
+		    smallest=data[z];
+		    n=z;}
+	    }
+	    data[n]=data[i];
+	    data[i]=smallest;	    
+	}
+    }
+
+    public void bSort(){
+	for (int i=0; i<data.length; i++){
+	    for (int z=0; z<data.length-1; z++){
+		String str;
+		if (data[z].compareTo(data[z+1])>0){
+		    str=data[z];
+		    data[z]=data[z+1];
+		    data[z+1]=str;}
+	    }
+	}
+    }
+    
     public String toString(){
 	String Answer = "";
 	for (int i=0;i<data.length;i++){
@@ -68,6 +95,7 @@ public  class Sarray{
     last++;
     }
 
+    
     public String get(int index){
     if (index>data.length){
         throw new ArrayIndexOutOfBoundsException();}
@@ -99,34 +127,37 @@ public  class Sarray{
     public static void main(String[] args){
 	Sarray V = new Sarray();
 	//System.out.println(V);
-	System.out.println(V.add("aa"));
-	System.out.println(V.add("ab"));
-	System.out.println(V.add("bb"));
-	System.out.println(V.add("bc"));
-	System.out.println(V.add("cc"));
-	System.out.println(V.add("cd"));
-	System.out.println(V.add("dd"));
-	System.out.println(V.add("de"));
-	System.out.println(V.add("ee"));
-	System.out.println(V.add("ef"));
-	System.out.println(V.add("ff"));
-	System.out.println(V.add("aa"));
-	System.out.println(V.add("ab"));
-	System.out.println(V.add("bb"));
-	System.out.println(V.add("bc"));
-	System.out.println(V.add("cc"));
-	System.out.println(V.add("cd"));
-	System.out.println(V.add("dd"));
-	System.out.println(V.add("de"));
-	System.out.println(V.add("ee"));
-	System.out.println(V.add("ef"));
-	System.out.println(V.add("ff"));
+	V.add("aa");
+	V.add("ab");
+	V.add("bb");
+	V.add("bc");
+	V.add("cc");
+	V.add("cd");
+	V.add("dd");
+	V.add("de");
+	V.add("ee");
+	V.add("ef");
+	V.add("ff");
+        V.add("aa");
+	V.add("ab");
+	V.add("bb");
+	V.add("bc");
+	V.add("cc");
+	V.add("cd");
+	V.add("dd");
+	V.add("de");
+	V.add("ee");
+	V.add("ef");
+	V.add("ff");
 	//System.out.println(V.size());
 	//System.out.println(V.get(0));
 	//System.out.println(V.add("go and die"));
 	//System.out.println(V.remove(0));
-	System.out.println(V);
-	V.iSort();
-	System.out.println(V);
+	//System.out.println(V);
+	//V.bSort(); //.062-.066
+	//V.sSort(); //.057-.069
+	//V.iSort();
+	
+	//System.out.println(V);
     }
 }
