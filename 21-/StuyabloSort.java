fcc83c6ed5ca134a825ab87629f45fedd47a774a
rong.yu <stuy.rong.yu@gmail.com>
2014-12-11 08:21:40 -0500

    I fixed the problem!

--- 21-/StuyabloSort.java
+++ 21-/StuyabloSort.java
@@ -13,20 +13,11 @@ public class StuyabloSort extends baseChar implements Comparable{
 	for (int i=0; i<BCA.length; i++){
 		BCA[i]= new baseChar();}
 	for (int i=0; i<BCA.length; i++){
-		System.out.println(BCA[i]);
+	    //System.out.println(BCA[i]);
 	}
 
-	/* this part doesn't work, it points to the period and says that
-	it cannot find the symbol 
-	for (int i=0; i<BCA.length-1; i++){
-		System.out.println(BCA[i].compareTo(BCA[i+1]));}
-	*/
 
 	Arrays.sort(BCA); 
-	//This doesn't work, says baseChar cannot be cast to java.lang.comparable
-	//But this is exactly the same as the classwork, I can't find the problem
-
-
 	for (int i=0; i<5; i++){
 		System.out.println(BCA[i]);
 	}
