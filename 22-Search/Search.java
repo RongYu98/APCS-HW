de42d5b63c14b70e4ca2b7d8669798ffb295ce3e
rong.yu <stuy.rong.yu@gmail.com>
2014-12-11 08:40:43 -0500

    classWork

---
+++ 22-Search/Search.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Search{
+
+    private ArrayList<Integers> AI;
+
+    public LSearch(ArrayList<Integers> AI){
+	for (int i=AI.length; 
+    }
+	
+
+}
